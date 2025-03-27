8c8368d76d154193668b6c624688e7be2240b7fc
Sangameswaran A <asangame@amazon.com>
2025-03-28 03:40:53 +0530

    Minor UI enhancements in Login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -21,21 +21,21 @@ export default class extends React.Component {
     }
 
     getGoogleLoginButton() {
-        return (
+        return (<div className="d-flex justify-content-center">
             <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
                 <GoogleLogin onSuccess={this.onSuccess} onError={this.onError} />
             </GoogleOAuthProvider>
-        );
+        </div>);
     }
 
     render() {
-        return (
+        return (<div className="bg-dark d-flex" style={{width: "100vw", height: "100vh"}}>
             <div className="container text-center mt-5">
                 <div className="card p-4 shadow-sm mx-auto" style={{ maxWidth: "400px" }}>
-                    <h2 className="mb-3">Finance Tracker</h2>
+                    <h2 className="mb-3">Welcome to Finance Tracker</h2>
                     {this.getGoogleLoginButton()}
                 </div>
             </div>
-        );
+        </div>);
     }
 }
