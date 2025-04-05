2adeab66769daf447e8d39147aadb4a386023e7a
Sangameswaran A <75067988+sangamapps@users.noreply.github.com>
2025-04-05 09:08:32 +0530

    Fix: Missed to check whether applied rule is skipped or not

--- src/utils/ruleUtil.js
+++ src/utils/ruleUtil.js
@@ -4,7 +4,7 @@ export default {
         rules.forEach(rule => {
             const { _id, contains, tag } = rule;
             transaction.appliedRules = transaction.appliedRules || {};
-            if (transaction.appliedRules[_id]) return;
+            if (transaction.appliedRules[_id] == 0) return;
             const description = _.lowerCase(transaction.description);
             const keywords = _.split(_.lowerCase(contains), ",");
             if (_.some(keywords, (word) => description.includes(word))) {
