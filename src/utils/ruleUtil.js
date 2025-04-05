3f909c0cb33c0f0f4e2765948fe85b9c96ba5aaf
Sangameswaran A <75067988+sangamapps@users.noreply.github.com>
2025-04-05 09:04:54 +0530

    Fix: Rules not getting reset when a rule is already applied

--- src/utils/ruleUtil.js
+++ src/utils/ruleUtil.js
@@ -4,7 +4,7 @@ export default {
         rules.forEach(rule => {
             const { _id, contains, tag } = rule;
             transaction.appliedRules = transaction.appliedRules || {};
-            if (_id in transaction.appliedRules) return;
+            if (transaction.appliedRules[_id]) return;
             const description = _.lowerCase(transaction.description);
             const keywords = _.split(_.lowerCase(contains), ",");
             if (_.some(keywords, (word) => description.includes(word))) {
